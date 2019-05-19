051cb480689396e9e29ce261a87ea58586ab6aab
AlvesDario <dario.alves@fatec.sp.gov.br>
2019-05-19 13:46:20 -0300

    Initial commit

---
+++ siga/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+# from .models import Carro
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+# Create your views here.
+# httpresponse should be given as parameters the path to the html file of the page
+
+def index(request):
+    return render(request, 'siga/index.html')
+
+def alunos(request):
+    return render(request, 'siga/alunos.html')
+
+# def login(request):
+#     return render(request, 'acme/login.html', {'css': 'acme/login.css'})
+
+# def signup(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data.get('username')
+#             messages.success(request, f'Account created for user {username}')
+#             return redirect('index')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'acme/signup.html', { 'form': form })
+
+# def cars(request):
+#     return HttpResponse('cars')
+
+# def offers(request):
+#     return HttpResponse('offers')
+
+# def about(request):
+#     return HttpResponse('about')
+
+# def account(request):
+#     return HttpResponse('account')
+
+# def get_cars(request):
+#     carros = Carro.objects.all()
+# return render(request, 'acme/carros.html', {'cars': carros})
